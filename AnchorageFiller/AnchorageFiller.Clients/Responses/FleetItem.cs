e2cd910850e8b9977ad04b9c82fd1c83e95a2f9a
Fredrik <50200428+fredrik-instech@users.noreply.github.com>
2026-01-06 10:51:58 +0100

    Cleanup

---
+++ AnchorageFiller/AnchorageFiller.Clients/Responses/FleetItem.cs
@@ -0,0 +1,3 @@
+﻿namespace AnchorageFiller.Clients.Responses;
+
+public record FleetItem(Size SingleShipSize, string ShipDesignation, int ShipCount);
--- AnchorageFiller/AnchorageFiller.Clients/Responses/GetFleetResponse.cs
+++ AnchorageFiller/AnchorageFiller.Clients/Responses/GetFleetResponse.cs
@@ -1,7 +1,3 @@
 ﻿namespace AnchorageFiller.Clients.Responses;
 
 public record GetFleetResponse(Size AnchorageSize, IReadOnlyList<FleetItem> Fleets);
-
-public record FleetItem(Size SingleShipDimensions, string ShipDesignation, int ShipCount);
-
-public record Size(int Width, int Height);
